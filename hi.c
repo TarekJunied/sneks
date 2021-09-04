ba8db0d60722163f51f5007842d7b4225e98d67b
TarekJunied <77919403+TarekJunied@users.noreply.github.com>
2021-09-04 11:24:31 +0200

    added sum function

--- hi.c
+++ hi.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int sum (int a, int b) 
+{	
+	return a + b;	
+)
+
+
 int main (void)
 {
 	printf("Hello World this code was written in terminal\n");
-
+	printf("The sum is %d", sum(2,3));
+	
 
 	return 0;
 }
